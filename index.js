1539b162359328dc110aee841191c3099ad16d28
unknown <anamika362890@gmail.com>
2022-11-09 01:48:30 +0600

    SingleServices

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,13 @@ async function run() {
             const cursor = foodCollection.find(query);
             const ServiceFoods = await cursor.toArray();
             res.send(ServiceFoods);
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await foodCollection.findOne(query);
+            res.send(service);
         })
 
 

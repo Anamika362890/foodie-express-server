b8dc1c3fc6fe674c401bb2e5d64759fbca0ad841
unknown <anamika362890@gmail.com>
2022-11-10 10:30:43 +0600

    foodServiceAdd

--- index.js
+++ index.js
@@ -19,8 +19,17 @@ async function run() {
     try {
 
         const foodCollection = client.db('foodieExpress').collection('foods');
+        const foodDetailsCollection = client.db('foodieExpress').collection('foodDetails');
+
 
         //data send to database
+
+        app.post('/service', async (req, res) => {
+            const foods = req.body;
+            const result = await foodCollection.insertOne(foods);
+            res.send(result);
+        })
+
         app.get('/foods', async (req, res) => {
             const query = {};
             const cursor = foodCollection.find(query);
@@ -39,6 +48,40 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const service = await foodCollection.findOne(query);
             res.send(service);
+        })
+
+
+
+        app.get('/foodDetails', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = foodDetailsCollection.find(query);
+            const foodDetail = await cursor.toArray();
+            res.send(foodDetail);
+        })
+
+        app.post('/foodDetails', async (req, res) => {
+            const foodDetail = req.body;
+            const result = await foodDetailsCollection.insertOne(foodDetail);
+            res.send(result);
+        })
+        app.put('/foodDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const doc = req.body;
+            const option = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: doc.comment
+
+                }
+            }
+            const result = await foodDetailsCollection.updateOne(filter, updatedDoc, option);
+            res.send(result);
         })
 
 
